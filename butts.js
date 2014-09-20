68641c351e48d141238e7f7f963670090fbc09e2
sagax <sagax@inbox.ru>
2014-09-21 01:30:47 +0400

    исправлена работа команды butts. поиск идет по всему диапазону возможных совпадений. производится проверка найденой ссылки

--- butts.js
+++ butts.js
@@ -1,21 +1,47 @@
+var request = require('request');
+var cheerio = require('cheerio');
 module.exports = {
   title: 'butts',
   match: /^!попка|^!butts|^!жопка/ig,
   onMsg: function (data) {
     /* !попка */
-    /* TODO добавить проверку наличия картинки по сгенерированной ссылке */
     with(data) {
       if (message.match(this.match)) {
+        var num, max, lap, search = who;
         var params = message.trim().split(' ');
         params.shift();
+        if (params.length) {
+          search = params.join(' ');
+        }
         setTimeout(function () {
-          var num = ((Math.random() * 10000) % 01120 | 0).toString();
-          num = (Array(6 - num.length)).join("0") + num;
-          var search = who;
-          if (params.length) {
-            search = params.join(' ');
-          }
-          bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg");
+          request('http://obutts.ru', function (error, response, body) {
+            var $ = cheerio.load(body);
+            max = $('div#main').children().first().attr('id').split('s')[1];
+          });
+          var gen_butts_link = function () {
+            num = ((Math.random() * 10000) % max | 0).toString();
+            num = (Array(6 - num.length)).join("0") + num;
+            check_butts();
+          };
+          var check_butts = function() {
+            request('http://media.obutts.ru/butts/' + num + ".jpg", function (error, response, body) {
+              var check;
+              var $ = cheerio.load(body);
+              check = $('title').text();
+              if (check !== '404 Not Found') {
+                bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg");
+                if (lap === 1) {
+                  console.log("сруза все нашли");
+                } else {
+                  console.log("пришлось потрудиться");
+                }
+              } else {
+                lap=lap+1;
+                gen_butts_link();
+              }
+            });
+          };
+          gen_butts_link();
         }, 1000);
       }
     }

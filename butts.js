99ed0e79a6cf68d6c2162fc2d7c2df4a01b904e3
sagax <sagax@inbox.ru>
2014-09-21 23:01:55 +0400

    убрано лишний разбор тела запроса, исправлен один из ответов бота

--- butts.js
+++ butts.js
@@ -13,37 +13,32 @@ module.exports = {
         if (params.length) {
           search = params.join(' ');
         }
-        setTimeout(function () {
-          request('http://obutts.ru', function (error, response, body) {
-            var $ = cheerio.load(body);
-            max = parseInt($('div#main').children().first().attr('id').split('s')[1]);
-          });
-          var gen_butts_link = function () {
-            num = ((Math.random() * 10000) % max | 0).toString();
-            num = (Array(6 - num.length)).join("0") + num;
-            check_butts();
-          };
-          var check_butts = function() {
-            request('http://media.obutts.ru/butts/' + num + ".jpg", function (error, response, body) {
-              var check;
-              var $ = cheerio.load(body);
-              check = $('title').text();
-              if (check !== '404 Not Found') {
-                if (lap === 1) {
-                  bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg");
-                } else {
-                  bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg" + " пришлось потрудиться");
-                }
-              } else if (lap < 3) {
-                lap = lap + 1;
-                gen_butts_link();
-              } else {
-                bot.say(channel, search + "сожалеем, мы сделали все возможное");
-              }
-            });
-          };
+        request('http://obutts.ru', function (error, response, body) {
+          var $ = cheerio.load(body);
+          max = parseInt($('div#main').children().first().attr('id').split('s')[1]);
           gen_butts_link();
-        }, 1000);
+        });
+        var gen_butts_link = function () {
+          num = ((Math.random() * 10000) % max | 0).toString();
+          num = (Array(6 - num.length)).join("0") + num;
+          check_butts();
+        };
+        var check_butts = function() {
+          request('http://media.obutts.ru/butts/' + num + ".jpg", function (error, response, body) {
+            if (response.statusCode !== 404) {
+              if (lap === 1) {
+                bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg");
+              } else {
+                bot.say(channel, search + ": http://media.obutts.ru/butts/" + num + ".jpg" + " пришлось потрудиться");
+              }
+            } else if (lap < 3) {
+              lap = lap + 1;
+              gen_butts_link();
+            } else {
+              bot.say(channel, search + ": сожалеем, мы сделали все возможное");
+            }
+          });
+        };
       }
     }
   }
